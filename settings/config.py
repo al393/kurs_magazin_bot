4c8805391164b442d09e12abb5f63983974a89b9
al393 <al393@mail.ru>
2022-07-20 10:40:54 +0300

    Add config

---
+++ settings/config.py
@@ -0,0 +1,54 @@
+import os
+from emoji import emojize
+from token import MY_TOKEN
+
+TOKEN = MY_TOKEN
+# имя БД
+NAME_DB = 'products.sqlite'
+# версия приложения
+VERSION = '0.0.1'
+# автор
+AUTHOR = 'User'
+
+# родительская директория(этого файла)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# путь до базы данных(в этой же папке)
+DATABASE = os.path.join('sqlite:///'+BASE_DIR, NAME_DB)
+
+COUNT = 0
+
+#
+KEYBOARD = {
+    'CHOOSE_GOODS': emojize(':open_file_folder: Выбрать товар'),
+    'INFO': emojize(':speech_balloon: О магазине'),
+    'SETTINGS': emojize('⚙️ Настройки'),
+    'SEMIPRODUCT': emojize(':pizza: Полуфабрикаты'),
+    'GROCERY': emojize(':bread: Бакалея'),
+    'ICE_CREAM': emojize(':shaved_ice: Мороженое'),
+    '<<': emojize('⏪'),
+    '>>': emojize('⏩'),
+    'BACK_STEP': emojize('◀️'),
+    'NEXT_STEP': emojize('▶️'),
+    'ORDER': emojize('✅ ЗАКАЗ'),
+    'X': emojize('❌'),
+    'DOUWN': emojize('🔽'),
+    'AMOUNT_PRODUCT': COUNT,
+    'AMOUNT_ORDERS': COUNT,
+    'UP': emojize('🔼'),
+    'APPLAY': '✅ Оформить заказ',
+    'COPY': '©️'
+}
+
+# id категорий продуктов
+CATEGORY = {
+    'SEMIPRODUCT': 1,
+    'GROCERY': 2,
+    'ICE_CREAM': 3,
+}
+
+# названия команд
+COMMANDS = {
+    'START': "start",
+    'HELP': "help",
+}
+
